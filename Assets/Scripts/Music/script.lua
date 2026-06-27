5103ffaff16382bf9a58e58b8ec4ab3df4617e23
Antônio Lucas C Araújo <156518068+ALucas314@users.noreply.github.com>
2026-06-27 06:52:27 -0300

    Add files via upload

---
+++ Assets/Scripts/Music/script.lua
@@ -0,0 +1,17 @@
+local sound = Instance.new("Sound")
+
+sound.Name = "Sound"
+sound.SoundId = "rbxassetid://103438831225831"
+sound.Volume = 0.01
+sound.PlaybackSpeed = 1
+sound.Looped = true
+sound.Archivable = false
+
+sound.Parent = workspace
+
+sound:Play()
+
+
+
+
+
